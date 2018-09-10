7d4b857c11ffa1dcd348a91e133c61e93e1caba0
pr <pushpasriram328@gmail.com>
2018-09-09 22:42:57 -0400

    initial commit

---
+++ SCMAssignment3/src/com/prang/scm/assignment3/SieveOfErasthones.java
@@ -0,0 +1,36 @@
+package com.prang.scm.assignment3;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * Print all the prime numbers smaller than or equal to 'n'
+ */
+public class SieveOfErasthones {
+	
+	int numberInput;
+		
+	public List<Integer> computePrimes(int n) {
+		boolean prime[] = new boolean[n + 1];
+	    Arrays.fill(prime, true);
+	    for (int p = 2; p * p <= n; p++) {
+	        if (prime[p]) {
+	            for (int i = p * 2; i <= n; i += p) {
+	                prime[i] = false;
+	            }
+	        }
+	    }
+	    List<Integer> primeNumbers = new LinkedList<>();
+	    for (int i = 2; i <= n; i++) {
+	        if (prime[i]) {
+	            primeNumbers.add(i);
+	        }
+	    }
+	    return primeNumbers;
+	}
+	
+	
+	
+
+}
