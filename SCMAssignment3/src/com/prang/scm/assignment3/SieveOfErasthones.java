a93d2761cf0414fce24ebd33837b0c41ac8032fb
prang0001 <42127159+prang0001@users.noreply.github.com>
2018-09-11 22:30:31 -0400

    Update SieveOfErasthones.java

--- SCMAssignment3/src/com/prang/scm/assignment3/SieveOfErasthones.java
+++ SCMAssignment3/src/com/prang/scm/assignment3/SieveOfErasthones.java
@@ -12,7 +12,7 @@ public class SieveOfErasthones {
 		
 	public List<Integer> computePrimes(int n) {
 		
-	    System.out.println("Auto build checking");		
+	    System.out.println("Auto build checking 2");		
 	    boolean prime[] = new boolean[n + 1];
 	    Arrays.fill(prime, true);
 	    for (int p = 2; p * p <= n; p++) {
