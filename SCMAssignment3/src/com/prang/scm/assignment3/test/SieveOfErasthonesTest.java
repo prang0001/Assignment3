a3c43aade4eb193b0b8075e6b105ff4fdc830ac5
pr <pushpasriram328@gmail.com>
2018-09-11 22:11:00 -0400

    changes

---
+++ SCMAssignment3/src/com/prang/scm/assignment3/test/SieveOfErasthonesTest.java
@@ -0,0 +1,22 @@
+package com.prang.scm.assignment3.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.prang.scm.assignment3.SieveOfErasthones;
+
+
+public class SieveOfErasthonesTest {
+	
+	@Test
+	public void testComputePrimes() {
+		int n = 10;
+		List<Integer> primes = new SieveOfErasthones().computePrimes(n);
+		System.out.println(primes);
+		assertTrue(primes.contains(7));
+	}
+
+}
